3b1d832bc1fb3add7fce00c3c8d7b0063fc3a376
Eugene Korenevsky <kara-91@tut.by>
2020-07-04 19:05:04 +0300

    rename

--- src/main/java/com/spring/model/helpers/roomhelpers/searchhelper/search/roomsearch/RoomSearchBySitsAndPrice.java
+++ src/main/java/com/spring/model/helpers/roomhelpers/searchhelper/search/roomsearch/RoomSearchBySitsAndPrice.java
@@ -10,13 +10,13 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RoomSearch implements RoomSearchHelper {
+public class RoomSearchBySitsAndPrice implements RoomSearchHelper {
     private final Filter<Room, Integer> filter;
     private final FilterWithTwoParam<Room, Integer, Double> filterWithTwoParam;
     private final DaysMaker daysMaker;
 
-    public RoomSearch(Filter<Room, Integer> filter, FilterWithTwoParam<Room, Integer, Double> filterWithTwoParam,
-                      DaysMaker daysMaker) {
+    public RoomSearchBySitsAndPrice(Filter<Room, Integer> filter, FilterWithTwoParam<Room, Integer, Double> filterWithTwoParam,
+                                    DaysMaker daysMaker) {
         this.filter = filter;
         this.filterWithTwoParam = filterWithTwoParam;
         this.daysMaker = daysMaker;
