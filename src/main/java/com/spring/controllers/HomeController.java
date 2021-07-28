77046bb16bde7a54fbb481cb5ada2e86f236ba1d
Eugene Korenevsky <kara-91@tut.by>
2021-07-28 10:52:33 +0300

    ref: add space

--- src/main/java/com/spring/controllers/HomeController.java
+++ src/main/java/com/spring/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping(value = {"/", "/home"})
     public String showHomePage() {
-        return "index";
+         return "index";
     }
 
 }
